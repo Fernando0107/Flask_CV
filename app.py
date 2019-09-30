2983ab7f74f3c786cad16499ac4b1186d92dcfcc
Fernando González <rmfernando@outlook.com>
2019-09-30 15:23:10 -0600

    Work Experience created

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import yaml
 import os
 import optparse
 
-developer = os.getenv("DEVELOPER", "Me")
+developer = os.getenv("DEVELOPER", "User")
 environment = os.getenv("ENVIRONMENT", "development")
 
 app = Flask(__name__)
 
 info = yaml.load(open('info.yml', 'r'))
 infoAc = yaml.load(open('infoA.yml', 'r'))
+infoExp = yaml.load(open('infoEXP.yml', 'r'))
 
 @app.route('/info')
 def hello_world():
@@ -19,7 +20,7 @@ def hello_world():
 @app.route('/')
 def index():
     #return render_template('index.html')
-    return render_template("index.html", info=info['info'], developer=developer, infoAc=infoAc['infoA'])
+    return render_template("index.html", info=info['info'], developer=developer, infoAc=infoAc['infoA'], infoExp=infoExp['infoExp'])
 
 
 @app.route('/about', methods=['GET', 'POST'])
@@ -29,11 +30,16 @@ def about():
     #return render_template("about.html", info=info['info'])
 
 
-@app.route('/academic', methods=['GET', 'POST'])
+@app.route('/academic', methods=['GET'])
 def aca():
 
     return redirect("http://localhost:5000/#academic")
 
+
+@app.route('/work', methods=['GET'])
+def work():
+
+    return redirect("http://localhost:5000/#Work")
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True)
