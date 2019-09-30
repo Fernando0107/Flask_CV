12d69c9366f560655ec6cbfc693377c3a8149a62
Fernando González <rmfernando@outlook.com>
2019-09-29 20:58:45 -0600

    Flask App.py created.

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify
+import yaml
+
+app = Flask(__name__)
+
+info = yaml.load(open('info.yml', 'r'))
+
+
+@app.route('/info')
+def hello_world():
+    return jsonify(info)
+
+
+@app.route('/')
+def index():
+    #return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', info=info['info'])
+
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", debug=True)
